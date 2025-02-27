86c41f752363c87fd5826b643867add89855bff2
skr18 <sujeetrath123@gmail.com>
2025-02-27 21:46:02 +0530

    27-02-2025|9.45|sujeet|fixed moble view issue

--- src/components/Product/Software.tsx
+++ src/components/Product/Software.tsx
@@ -24,14 +24,14 @@ const Software = () => {
             <div className="flex items-center gap-2">
               <img className='max-w-20 rounded-full' src={logo} alt="logo" />
             </div>
-              <div className="md:flex items-center gap-8">
-                <a href="/software" className="text-gray-300 hover:text-white transition-colors">Mat Visualizer</a>
-                <a href="/software" className="text-gray-300 hover:text-white transition-colors">Other softwares</a>
-                <a href="/" className="text-gray-300 hover:text-white transition-colors">Home</a>
-              </div>
+            <div className="flex flex-col gap-1 md:flex-row md:gap-8 items-cente">
+              <a href="/software" className="text-gray-300 hover:text-white transition-colors">Mat Visualizer</a>
+              <a href="/software" className="text-gray-300 hover:text-white transition-colors">Other softwares</a>
+              <a href="/" className="text-gray-300 hover:text-white transition-colors">Home</a>
+            </div>
           </div>
-          </div>
-        </nav>
+        </div>
+      </nav>
 
 
         <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center p-4">
