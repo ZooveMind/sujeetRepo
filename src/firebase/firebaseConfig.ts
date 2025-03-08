a7ac16a54a182663a06ded54cda074d3a3cec42c
skr18 <sujeetrath123@gmail.com>
2025-03-09 01:16:25 +0530

    try to fix

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
-
+console.log('FIREBASE CONFIG IN PRODUCTION:', firebaseConfig);
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
